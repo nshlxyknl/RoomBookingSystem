d9b93840a6b78b37c9d17e232568fc7fe9796723
nis chal <khanaln07@gmail.com>
2026-02-06 21:54:19 +0545

    fix(google): correct import path for google controller

--- backend/routes/google.js
+++ backend/routes/google.js
@@ -1,6 +1,6 @@
 const express = require(`express`)
 const router=express.Router()
-const { google } = require("../controllers/googlecontrollers")
+const { google } = require("../controllers/googlecontroller")
 
 
 router.post(`/google`,google)
