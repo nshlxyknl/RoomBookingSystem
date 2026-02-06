ca02a9730520bf6ab7a9c226dd0a458012678647
nis chal <khanaln07@gmail.com>
2026-02-06 21:58:32 +0545

    fix(main): add missing GoogleOAuthProvider import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { AuthProvider } from './context/AuthProvider'
 import { TabProvider } from './context/TabProvider'
 import { RoomProvider } from './context/RoomProvider'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const CLIENT_ID="800452047417-g9gm7pggs6386ma9bvgce0t7nudbal8g.apps.googleusercontent.com"
 
